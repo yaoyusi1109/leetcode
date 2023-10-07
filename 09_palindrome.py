3e5409f5dd3ba50eddb997aa19ff57b2507ca40b
Yusi <yao.yusi@outlook.com>
2023-10-06 21:24:28 -0400

    09

---
+++ 09_palindrome.py
@@ -0,0 +1,10 @@
+# 9 Palindrome Number
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        if x == x[::-1]:
+            return True
+        else:
+            return False
+        
