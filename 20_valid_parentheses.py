b6bfb3599f88dd6ac152664a73121304eda290fb
Yusi <yao.yusi@outlook.com>
2023-10-08 22:30:28 -0400

    swap file

---
+++ 20_valid_parentheses.py
@@ -0,0 +1,19 @@
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {')':'(', '}':'{', ']':'['}
+        
+        for char in s:
+            if char in closeToOpen:
+                if stack and stack[-1] == closeToOpen[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        return True if not stack else False
+        
